323045bbf666ef69dfbe9b87edd7db479c15db47
Anna78990 <takasuanna1990@gmail.com>
2022-01-13 15:12:46 +0000

    for non conflict

--- monty.c
+++ monty.c
@@ -66,10 +66,6 @@ void check_instruction(stack_t **m_stack, unsigned int line_number)
 		{"swap", swap},
 		{"add", add},
 		{"nop", nop},
-		{"sub", sub},
-		{"div", _div},
-		{"mul", mul},
-		{"mod", mod},
 		{NULL, NULL},
 	};
 
