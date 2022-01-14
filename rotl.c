f37ae3b8566fce06a42956ccef4c2eda551ce859
Anna78990 <takasuanna1990@gmail.com>
2022-01-14 11:23:30 +0000

    rotl

--- rotl.c
+++ rotl.c
@@ -10,16 +10,35 @@
 void rotl(stack_t **stack, unsigned int line_number)
 {
 	stack_t *last, *cln, *tmp;
+	int counter = 0, t;
 	(void)line_number;
 
+	if ((*stack)->next == NULL)
+	{
+	}
+	else
+	{
 	last = *stack;
 	cln = *stack;
 	while (last->next)
+	{
 		last = last->next;
-	tmp = cln->next;
-	cln->next->prev = NULL;
-	cln->prev = last;
-	last->next = cln;
-	cln->next = NULL;
-	(*stack) = tmp;
+		counter++;
+	}
+	if (counter == 1)
+	{
+		t = (*stack)->n;
+		(*stack)->n = (*stack)->next->n;
+		(*stack)->next->n = t;
+	}
+	else
+	{
+		tmp = cln->next;
+		cln->next->prev = NULL;
+		cln->prev = last;
+		last->next = cln;
+		cln->next = NULL;
+		(*stack) = tmp;
+	}
+	}
 }
